47056e711d368023f22644dfd94d369d2f65c37c
sheep <sheep@DESKTOP-RHEAN0C>
2020-10-10 16:41:03 +0200

    It seems like its working for a single initial explanation

--- myProgram.py
+++ myProgram.py
@@ -1,6 +1,6 @@
 import os, glob, re, shutil
 
-inputOntology = "datasets/abstract_uvw.owl"
+inputOntology = "datasets/simple-deep-ontology.owl"
 
 inputSubclassStatements = "datasets/subClasses.nt" # this file can be generated using the second command (saveAllSubClasses)
 
@@ -86,23 +86,69 @@ def write_variables_to_file(path, name, list_of_results):
         f.close()
         var_counter += 1
 
+def forget_explanation(new_file_path, method, var_counter):
+    if(var_counter > 2):
+
+        owl_counter = 1
+        # For each result-#.owl file
+        for file in glob.glob(new_file_path + "/result*.owl"):
+            
+            newest_file_path = new_file_path + "/" + str(owl_counter)
+
+            # for each subsequent explanation, create a folder
+            os.makedirs(newest_file_path)
+
+            # Run Save subclasses and explanations on the new .owl file
+            os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + file)
+            os.system('java -jar kr_functions.jar ' + 'saveAllExplanations' + " " + file + " " + inputSubclassStatements)
+
+            # Move all .omn file to respective folder
+            omn_counter = 1
+            for omn_file in glob.glob(new_file_path + "/*.omn"):
+                #os.makedirs(newest_file_path + "/" + str(omn_counter))
+                new_omn_file_path = newest_file_path + "/" + str(omn_counter) + ".omn"
+                shutil.move(omn_file, new_omn_file_path)
+
+                parsed_vars = parseOMN(new_omn_file_path)
+                write_variables_to_file(newest_file_path + "/", str(omn_counter), parsed_vars)
+
+                # For each variable from the explanation
+                var_counter = 1
+                for var in glob.glob(newest_file_path + "/*.txt"):
+                    os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + 
+                    new_omn_file_path + ' --method ' + method  + ' --signature ' + var)
+                    shutil.move("result.owl", newest_file_path + "/result-" + str(var_counter) + ".owl")
+                    var_counter += 1
+
+                forget_explanation(newest_file_path, method, var_counter - 1) # Extract one from the var counter because it starts with 1 for naming pourposes
+
+                omn_counter += 1
+            
+            owl_counter += 1
+
 # For each initial explanation
-for file in glob.glob("datasets/exp*"):
-    file_name = file.split("\\")[1].split('.')[0]
+#for file in glob.glob("datasets/exp*"):
+file = "datasets\\exp1-1.omn"
+file_name = file.split("\\")[1].split('.')[0]
 
-    # Now for each initial explanation, create a folder
-    os.makedirs("results/" + file_name)
-    new_file_path = "results/" + file_name + "/" + file_name + ".owl"
-    shutil.move(file, new_file_path)
+# Now for each initial explanation, create a folder
+os.makedirs("results/" + file_name)
+new_file_path = "results/" + file_name + "/" + file_name + ".owl"
+shutil.move(file, new_file_path)
 
-    parsed_vars = parseOMN(new_file_path)
-    write_variables_to_file("results/" + file_name + "/", file_name, parsed_vars)
+parsed_vars = parseOMN(new_file_path)
+write_variables_to_file("results/" + file_name + "/", file_name, parsed_vars)
 
-    # For each variable from the explanation
-    var_counter = 1
-    for var in glob.glob("results/" + file_name + "/*.txt"):
-        os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + 
-        new_file_path + ' --method ' + method  + ' --signature ' + var)
-        shutil.move("result.owl", "results/" + file_name + "/" + file_name + "-result-" + str(var_counter) + ".owl")
-        var_counter += 1
-        # recheck for variables before you do lethe again
+# For each variable from the explanation
+var_counter = 1
+for var in glob.glob("results/" + file_name + "/*.txt"):
+    os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + 
+    new_file_path + ' --method ' + method  + ' --signature ' + var)
+    shutil.move("result.owl", "results/" + file_name + "/result-" + str(var_counter) + ".owl")
+    var_counter += 1
+
+# Go in the recursive forgetter, Only if there are more then two variables
+forget_explanation("results/" + file_name, method, var_counter)
+
+
+    # recheck for variables before you do lethe again
